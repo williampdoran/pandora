7f2b4a3f9691525e349ac1f990c53396d164b8a8
wdoran <wdoran@tenable.com>
2021-10-23 18:51:29 +0100

    add random data generation code

---
+++ PartOne/src/main/python/data_generator/data_generator.py
@@ -0,0 +1,36 @@
+import csv
+
+from faker import Faker
+import numpy as np
+
+class DataGenerator:
+    def __init__(self, dirname):
+        self.dirname = dirname
+        self.fake = Faker()
+        self.product_categories = ["charms", "bracelets", "rings", "null"]
+
+    def generateOrders(self, numOrders, product_ids, numCustomers):
+        customerIds = self.generateIds(numCustomers)
+        orders = [{'order_id': self.fake.uuid4(), 'customer_id' : np.random.choice(customerIds), 'product_id' : np.random.choice(product_ids), 'order_date' :self.fake.date_this_year(after_today=True).isoformat()} for x in range(numOrders)]
+        return orders
+
+    def generateProducts(self, productIds):
+        products = [{'product_id' :x,'product_category': np.random.choice(self.product_categories, p=[0.2, 0.4, 0.3, 0.1])} for x in productIds]
+        return products
+
+    def generateIds(self, numIds):
+        return [self.fake.uuid4() for i in range(numIds)]
+
+    def writeTables(self, numOrders, numProducts, numCustomers):
+        productIds = self.generateIds(numProducts)
+        products = self.generateProducts(productIds)
+        orders = self.generateOrders(numOrders, productIds, numCustomers)
+        self._writeTable('orders', orders)
+        self._writeTable('products', products)
+
+    def _writeTable(self, filename, dataset):
+        fieldnames = dataset[0].keys()
+        with open(f'{self.dirname}/{filename}.csv', mode='w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(dataset)
---
+++ PartOne/src/test/python/generate_data_test.py
@@ -0,0 +1,57 @@
+import csv
+import datetime
+import os
+import uuid
+from datetime import date
+
+from data_generator.data_generator import DataGenerator
+
+class TestCase:
+    dirname = "src/test/resources"
+    dataGenerator = DataGenerator(dirname)
+
+    def test_generates_file(self):
+        num_orders = 1000
+        num_products = 10
+        num_customers = 20
+        self.dataGenerator.writeTables(num_orders, num_products, num_customers)
+        # print(os.getcwd())
+        with open(f'{self.dirname}/orders.csv') as f:
+            reader = csv.DictReader(f)
+            assert reader.fieldnames == ['order_id', 'customer_id', 'product_id', 'order_date']
+            rows = [row for row in reader]
+            assert len(rows) == num_orders
+        with open(f'{self.dirname}/products.csv') as f:
+            reader = csv.DictReader(f)
+            assert reader.fieldnames == ['product_id', 'product_category']
+            rows = [row for row in reader]
+            assert len(rows) == num_products
+
+    def test_product_ids(self):
+        fake_products = self.dataGenerator.generateProducts(self.dataGenerator.generateIds(10))
+        assert len(fake_products) == 10
+        uuid = fake_products[0]['product_id']
+        assert self.is_valid_uuid(uuid)
+        assert fake_products[0]['product_category'] in ["charms", "bracelets", "rings", "null"]
+
+    def test_orders(self):
+        expected_product_ids = ['a', 'b', 'c', 'd']
+        fake_orders = self.dataGenerator.generateOrders(numOrders=10, product_ids=expected_product_ids, numCustomers=20)
+        print(fake_orders)
+        assert len(fake_orders) == 10
+
+        uuid = fake_orders[0]['order_id']
+        assert self.is_valid_uuid(uuid)
+
+        product_id = fake_orders[0]['product_id']
+        assert product_id in expected_product_ids
+
+        order_date = date.fromisoformat(fake_orders[0]['order_date'])
+        assert datetime.date(2021, 12, 31) > order_date > datetime.date(2021, 1, 1)
+
+    def is_valid_uuid(self, uuid_to_test):
+        try:
+            uuid_obj = uuid.UUID(uuid_to_test)
+        except ValueError:
+            return False
+        return str(uuid_obj) == uuid_to_test
